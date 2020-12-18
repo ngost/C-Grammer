f01d2f9237dc422a0436c7b3b2a9a5cb17cd5810
ngost <hm5207@gmail.com>
2020-12-18 15:19:35 +0900

    git init

---
+++ _Net_Grammer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Net_Grammer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# with easyJin");
+        }
+    }
+}
